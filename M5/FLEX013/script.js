5af9a71f049a575613411e932d90a23a31cc7a13
Marostega <marcosmarostega@hotmail.com>
2025-01-30 21:39:28 -0300

    Menu Flex

---
+++ M5/FLEX013/script.js
@@ -0,0 +1,15 @@
+function abrirMenu() {
+    if (menu.style.display == "flex") {
+        menu.style.display = "none"
+    } else {
+        menu.style.display = "flex"
+    }
+}
+
+// function mudarTamanho () {
+//     if (window.innerWidth >= 768) {
+//         menu.style.display = "block"
+//     } else {
+//         menu.style.display = "none"
+//     }
+// }
