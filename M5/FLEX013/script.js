28f13e5014b6732c4d43eb7082acb03a85572403
Marostega <marcosmarostega@hotmail.com>
2025-02-03 18:19:46 -0300

    My Favorite Pet

--- M5/FLEX013/script.js
+++ M5/FLEX013/script.js
@@ -6,10 +6,16 @@ function abrirMenu() {
     }
 }
 
-// function mudarTamanho () {
-//     if (window.innerWidth >= 768) {
-//         menu.style.display = "block"
-//     } else {
-//         menu.style.display = "none"
-//     }
-// }
+function esconderAparecerMenuAoMudarTamanho() {
+    if (window.innerWidth >= 1050) {
+        menu.style.display = "flex"
+    } else {
+        menu.style.display = "none"
+    }
+}
+
+window.onload = function() {
+    esconderAparecerMenuAoMudarTamanho();
+};
+
+// Correção de bug: Ao ocultar o menu pelo botão "hamburguer" e depois aumentar o tamanho da tela, o menu permanecia escondido sem opção de reexibição. Agora, ao redimensionar a tela para mais de 1000px, o menu será exibido automaticamente, desconsiderando a configuração anterior.
